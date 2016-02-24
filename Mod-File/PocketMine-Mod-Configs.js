be91092b36f3289a8d49dd8f7e74348bda88563b
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-02-24 15:52:50 -0300

    Configs Beta

---
+++ Mod-File/PocketMine-Mod-Configs.js
@@ -0,0 +1,27 @@
+var PMRunMod = {};
+
+function newLevel(){
+		PMRunMod.saveMainSettings();
+
+}
+
+var settingsPath = android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/games/PocketMineMod/";
+var settingsPathYML = android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/games/PocketMineMod/";
+
+PMRunMod.saveMainSettings = function() {
+    java.io.File(settingsPath).mkdirs();
+    var newFile = new java.io.File(settingsPath, "server.properties");
+    var newFileYML = new java.io.File(settingsPath, "server.yml");
+    newFile.createNewFile();
+    newFileYML.createNewFile();
+    }
+    PMRunMod.loadMainSettings = function() {
+    if(!java.io.File(settingsPath + "server.properties").exists())
+        return;
+    var file = new java.io.File(settingsPath + "server.properties");
+}
+    PMRunMod.loadMainSettingsYML = function() {
+    if(!java.io.File(settingsPath + "server.yml").exists())
+        return;
+    var file = new java.io.File(settingsPath + "server.yml");
+}
